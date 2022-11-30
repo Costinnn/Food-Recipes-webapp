9fd1a0ba0dc89a5b282a03ce0c24575681c30f4e
Costinnn <crauciuc.costin@yahoo.com>
2022-11-30 14:31:19 +0200

    useContext and Reducers for theme change

--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import Create from "./pages/create/Create";
 import Search from "./pages/search/Search";
 import Recipe from "./pages/recipe/Recipe";
 import Navbar from "./components/Navbar";
+import ThemeSelector from "./components/ThemeSelector";
+import { useTheme } from "./hook/useTheme";
 
 function App() {
+  const { mode } = useTheme();
+
   return (
-    <div className="App">
+    <div className={`App ${mode}`}>
       <Navbar />
+      <ThemeSelector />
       <Routes>
         <Route index path="/" element={<Home />}></Route>
         <Route path="/create" element={<Create />}></Route>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,30 @@
-import './Navbar.css';
-import {Link} from 'react-router-dom'
-import Searchbar from './Searchbar';
+import "./Navbar.css";
+import { Link } from "react-router-dom";
+import Searchbar from "./Searchbar";
+// import { useContext } from 'react';
+// import { ThemeContext } from '../context/ThemeContext';
+import { useTheme } from "../hook/useTheme";
 
 const Navbar = () => {
-    return(<div className='navbar'>
-        <nav>
-            <Link to='/' className='brand'>
-            <h1>Cooking Recipes</h1>
-            </Link>
-            <Searchbar/>
-            <Link to='/create'>
-            <h1>Create Recipe</h1>
-            </Link>
-        </nav>
-    </div>)
-}
+  // version 1
+  // const {color} = useContext(ThemeContext);
+
+  //version 2 with new hook 'useTheme()'
+  const { color} = useTheme();
+
+  return (
+    <div className="navbar" style={{ background: color }}>
+      <nav>
+        <Link to="/" className="brand">
+          <h1>Cooking Recipes</h1>
+        </Link>
+        <Searchbar />
+        <Link to="/create">
+          <h1>Create Recipe</h1>
+        </Link>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
